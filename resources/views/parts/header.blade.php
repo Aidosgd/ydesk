2b36adb01b9544a3578c2ce5922fb4e779d182b4
Aidosgd <aidosgd@gmail.com>
2017-05-18 18:27:43 +0600

    fix

--- resources/views/parts/header.blade.php
+++ resources/views/parts/header.blade.php
@@ -1,50 +1,48 @@
 <header id="top-bar" class="navbar-fixed-top animated-header">
-    <div class="container">
-        <div class="navbar-header">
-            <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
-                <span class="sr-only">Toggle navigation</span>
-                <span class="icon-bar"></span>
-                <span class="icon-bar"></span>
-                <span class="icon-bar"></span>
-            </button>
-            <div class="navbar-brand">
-                <a href="/" style="margin-top: 0">
-                    <img src="/images/logo.jpg" alt="">
-                </a>
-            </div>
-            <ul class="nav navbar-nav">
-                @foreach($main_menu as $item)
-                    <li>
-                        <a href="/{{ $lang }}/{{ $item->link }}">{{ $item->node->title }}</a>
-                    </li>
-                @endforeach
+    <div class="navbar-header">
+        <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
+            <span class="sr-only">Toggle navigation</span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+        </button>
+        <div class="navbar-brand">
+            <a href="/" style="margin-top: 0">
+                <img src="/images/logo.jpg" alt="">
+            </a>
+        </div>
+        <ul class="nav navbar-nav">
+            @foreach($main_menu as $item)
+                <li>
+                    <a href="/{{ $lang }}/{{ $item->link }}">{{ $item->node->title }}</a>
+                </li>
+            @endforeach
+        </ul>
+    </div>
+    <nav class="collapse navbar-collapse navbar-right" role="navigation">
+        <div class="main-menu">
+            <ul class="nav navbar-nav navbar-right">
+                <li>
+                    <a href="#" class="Facebook">
+                        <i class="ion-social-facebook"></i>
+                    </a>
+                </li>
+                <li>
+                    <a href="#" class="Twitter">
+                        <i class="ion-social-twitter"></i>
+                    </a>
+                </li>
+                <li>
+                    <a href="#" class="Linkedin">
+                        <i class="ion-social-linkedin"></i>
+                    </a>
+                </li>
+                <li>
+                    <a href="#" class="Google Plus">
+                        <i class="ion-social-googleplus"></i>
+                    </a>
+                </li>
             </ul>
         </div>
-        <nav class="collapse navbar-collapse navbar-right" role="navigation">
-            <div class="main-menu">
-                <ul class="nav navbar-nav navbar-right">
-                    <li>
-                        <a href="#" class="Facebook">
-                            <i class="ion-social-facebook"></i>
-                        </a>
-                    </li>
-                    <li>
-                        <a href="#" class="Twitter">
-                            <i class="ion-social-twitter"></i>
-                        </a>
-                    </li>
-                    <li>
-                        <a href="#" class="Linkedin">
-                            <i class="ion-social-linkedin"></i>
-                        </a>
-                    </li>
-                    <li>
-                        <a href="#" class="Google Plus">
-                            <i class="ion-social-googleplus"></i>
-                        </a>
-                    </li>
-                </ul>
-            </div>
-        </nav>
-    </div>
+    </nav>
 </header>
